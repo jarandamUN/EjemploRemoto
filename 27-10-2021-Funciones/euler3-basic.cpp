337a3e319c1a2dce5245c4306a3db6b70a4c2fe3
Juan Carlos Aranda <jarandam@unal.edu.co>
2021-10-29 08:55:37 -0500

    Añadido suma de numeros primos, mayor optimización del código

--- 27-10-2021-Funciones/euler3-basic.cpp
+++ 27-10-2021-Funciones/euler3-basic.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <cmath>
 //Numero primo divisor más grande de N
 
 bool isprime(long long n ); //declaration
 long long int largestPrimeDivisor(long long int n);
+long long int sumaPrimeDivisors(long long int n);
 
 int main(void){
     
     const long long int N = 600851475147;
     std::cout << largestPrimeDivisor(N) << std::endl;
+    std::cout << sumaPrimeDivisors(N) << std::endl;
     
     return 0;
 }
 
 bool isprime(long long n){
     bool isprime = true;
-    for (long long int jj = 2; jj < n; jj++){
+    for (long long int jj = 2; jj <= std::sqrt(n); jj++){
                 if(n % jj == 0 ){
                     isprime = false;
                     break;
@@ -24,7 +27,7 @@ bool isprime(long long n){
 }
 
 long long int largestPrimeDivisor(long long int n){
-    long long int maxPrimeDivisor = 1;
+    long long int maxPrimeDivisor = n;
     for(long long int ii = 2; ii < n; ii++ ){
         if(n % ii == 0){
             maxPrimeDivisor = n/ii;
@@ -37,4 +40,21 @@ long long int largestPrimeDivisor(long long int n){
     }
 
     return maxPrimeDivisor;
+}
+
+long long int sumaPrimeDivisors(long long int n){
+
+    long long int result = 0;
+
+    for(long long int ii = 2; ii <= n; ii++ ){
+        if(n % ii == 0 && isprime(ii)){
+            
+            n /= ii;
+            result += ii;
+            std::cout << "Un divisor: " << ii << ", Suma de ellos: " << result << std::endl;            
+            
+        }
+    }
+    
+    return result;
 }
