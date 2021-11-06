3e9f692d9ac15fb746047d5799165274b6e56b45
Juan Carlos Aranda <jarandam@unal.edu.co>
2021-11-06 13:17:36 -0500

    Derivación numerica e Integracion numerica con trapezio y richardson

--- 3-11-2021-DerNumerica/deriv-forward.cpp
+++ 3-11-2021-DerNumerica/deriv-forward.cpp
@@ -2,19 +2,24 @@
 #include <cmath>
 
 using fptr = double(double);
+using fptrderiv = double(double, double, fptr);
 
 double fun(double x);
 double g(double x);
 double forward(double x, double h, fptr f);
+double central(double x, double h, fptr f);
+double richardson(double x, double h, fptr f, fptrderiv alg);
 
 int main(int argc, char **argv){
 
     std::cout.precision(15); std::cout.setf(std::ios::scientific);
     const double X = std::atof(argv[1]);
 
-    for(double h = 1.0e-1; h >= 1.0e-8; h /= 10.0){
+    for(double h = 1.0e-1; h >= 1.0e-15; h /= 10.0){
         std::cout << h << "\t"
-                  << std::fabs(1-forward(X, h, g)/std::cos(X)) << "\n";
+                  << std::fabs(1-forward(X, h, fun)/std::cos(X)) << "\t" // muestra el error producido entre nuestra aprox y el valor real de la derivada
+                  << std::fabs(1-central(X, h, fun)/std::cos(X)) << "\t"
+                  << std::fabs(1-richardson(X, h, fun, central)/std::cos(X)) << "\n";
     
     }
     
@@ -34,4 +39,18 @@ double g(double x){
 double forward(double x, double h, fptr f){ 
 
     return ((f(x+h)-f(x))/h);
+}
+
+double central(double x, double h, fptr f){
+
+    return ((f(x+h)-f(x-h))/(2*h));
+}
+
+double richardson(double x, double h, fptr f, fptrderiv alg){
+
+    double val1 = alg(x,h,f);
+    double val2 = alg(x,h/2,f);
+
+    return (4*val2-val1)/3;
+
 }
